94731f0951bcc9ee788b376cd9df70ee48236962
Sara Musella <123462665+smusella@users.noreply.github.com>
2024-11-10 11:42:03 +0000

    uu

--- Maps_Sets/D1Sets/Sets.java
+++ Maps_Sets/D1Sets/Sets.java
@@ -1,6 +1,8 @@
 package Maps_Sets.D1Sets;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 //Liste von Elemente -> Reihenfolge egal
@@ -11,7 +13,11 @@ public class Sets {
     public static void main(String[] args) {
      
     
-    Set<String> namen = new HashSet<>();
+    Set<String> namen = new HashSet<>(); 
+    /*Wenn man TreeSet benutzt werden die Namen in alphabetischer Reihenfolge ausgegeben
+      Wenn man LinkedHashSet benutzt werden die Namen in die Reihenfolge ausgegeben wie man sie schreibt
+      Schnell: HashSet > LinkedHashSet > TreeSet :Langsam
+    */
 
     namen.add("Max");
     namen.add("Moritz");
@@ -23,6 +29,27 @@ public class Sets {
     System.out.println(namen);
     System.out.println(namen.size()); //zeigt wie viele Elemente im Set sind
     System.out.println(namen.contains("Alex")); //pruft ob Alex im Set ist
+
+//-------------------------------------------------------------------------------------
+
+
+    List<Integer> Zahlenliste = new ArrayList<>(); //so werden gleiche Zahlen in der Liste trotzdem angezeigt 
+
+    Zahlenliste.add(0); 
+    Zahlenliste.add(1);
+    Zahlenliste.add(2);
+    Zahlenliste.add(3);
+    Zahlenliste.add(1);
+
+    System.out.println(Zahlenliste);
+
+
+    Set<Integer> ZahlenSet = new HashSet<>(Zahlenliste);// so werden gleiche Zahlen nur einmal angezeigt
+
+    /*ZahlenSet.addAll(Zahlenliste); -> alle Zahlen die in ZahleListe stehen werden auch hier verwendet
+     oder man schreibt es direkt in den Klammer der Konstruktor
+    */
+    System.out.println(ZahlenSet);
  }
    
 }
