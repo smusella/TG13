9943f96a3129e84251b2431a7b900b7a2ae74c64
Sara Musella <123462665+smusella@users.noreply.github.com>
2025-01-14 11:55:25 +0000

    hhhh

---
+++ K_nearest_algorithm/Dataobjekt.java
@@ -0,0 +1,9 @@
+public class Dataobjekt {
+
+    public Dataobjekt (int x, int y, int Klassen){
+        this.x = x;
+        this.y = y;
+        this.Klassen = Klassen;
+    }
+
+}
--- K_nearest_algorithm/KNN.java
+++ K_nearest_algorithm/KNN.java
@@ -1,4 +1,33 @@
+import java.util.List;
+
 public class KNN {
 
+    public static int knn(Dataobjekt neu, List<Dataobjekt> testdaten, int k){
+
+     return k;
+
+    }
+
+    public static void main(String[] args) {
+       
+        List<Dataobjekt> testdaten = new ArrayList<>();
+        testdaten.add(new Dataobjekt(1.0,5));
+        testdaten.add(new Dataobjekt(1.0,2.0));
+        testdaten.add(new Dataobjekt(1.0,2.0));
+        testdaten.add(new Dataobjekt(1.0,2.0));
+
+        Dataobjekt neu = new Dataobjekt(null,0)(2.0,2.0), -1);
+       
+    
+        int klasseNeu = knn(neu, testdaten, 3);
+
+        System.out.println(klasseNeu);
+    
+    
+    
+    }
+    
+    
+    
     
 }
